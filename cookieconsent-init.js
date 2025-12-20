df2a0494ddce262068cb16b7d45ca95a5b9583c5
Fit_my_pc <cedric.derksen08@gmail.com>
2025-12-20 17:53:29 +0100

    Fix: Button-Reihenfolge korrigiert und Banner quadratisch (480x480px)

--- cookieconsent-init.js
+++ cookieconsent-init.js
@@ -411,8 +411,8 @@ function initializeCookieConsent() {
         // TEST-MODUS: Cookie läuft sofort ab (-1 Tag), damit Banner immer erscheint
         cookie: { name: 'cc_cookie_fitmypc_v2', expiresAfterDays: -1 },
         guiOptions: {
-            consentModal: { layout: 'box', position: 'middle center', flipButtons: false },
-            preferencesModal: { layout: 'box', position: 'middle center', flipButtons: false }
+            consentModal: { layout: 'box', position: 'middle center', flipButtons: true },
+            preferencesModal: { layout: 'box', position: 'middle center', flipButtons: true }
         },
         onFirstConsent: () => { removeSiteBlocker(); applyPreferences(); resetInitialChecks(); },
         onConsent: () => { removeSiteBlocker(); applyPreferences(); resetInitialChecks(); },
